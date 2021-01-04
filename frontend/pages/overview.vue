7638c51a798c5be25c982b1f700626fdd99f83f0
yagorundy <yagorundy@gmail.com>
2021-01-04 21:56:53 +0200

    Added country selector

--- frontend/pages/overview.vue
+++ frontend/pages/overview.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    <h1>Overview</h1>
+    <select
+      id="country-select"
+      v-model="selectedCountryCode"
+    >
+      <option disabled :selected="!selectedCountryCode">Select a country</option>
+      <option
+        :key="index"
+        v-for="(country, index) in countries"
+        :value="country.code"
+        :selected="selectedCountryCode === country.code"
+      >{{ country.name }}</option>
+    </select>
+
+    {{ selectedCountryCode }}
+
     <google-map />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import GoogleMap from '../components/GoogleMap.vue'
+import Component from 'nuxt-class-component'
+import GoogleMap from '~/components/GoogleMap.vue'
+import countries from '~/static/countries.json'
 
-export default Vue.extend({
-  components: { GoogleMap }
-})
+@Component({})
+export default class extends Vue {
+  countries = countries
+  selectedCountryCode = ''
+}
 </script>
