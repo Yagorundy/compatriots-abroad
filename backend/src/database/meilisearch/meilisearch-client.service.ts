f8eb30c7b451edff6b589d9139357db8b30d3a67
yagorundy <yagorundy@gmail.com>
2021-02-10 22:04:00 +0200

    Updated meilisearch

--- backend/src/database/meilisearch/meilisearch-client.service.ts
+++ backend/src/database/meilisearch/meilisearch-client.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common'
 import { MeiliSearch } from 'meilisearch'
 import { MeilisearchConfigService } from '../../config/meilisearch/meilisearch-config.service'
 
-
 @Injectable()
 export class MeilisearchClientService extends MeiliSearch {
     constructor(meilisearchConfigService: MeilisearchConfigService) {
--- backend/src/infrastructure/meilisearch/meilisearch.service.ts
+++ backend/src/infrastructure/meilisearch/meilisearch.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { MeilisearchClientService } from '../../database/meilisearch/meilisearch-client.service'
-
-
 @Injectable()
 export class MeilisearchService {
     constructor(private meilisearchClientService: MeilisearchClientService) {}
 
     async getPublicKey() {
-        // TODO: uncomment when meili js is fixed
-        // const keys = await this.meilisearchClientService.getKeys()
-        // return keys.public
-        return await this.meilisearchClientService.listIndexes()
+        const keys = await this.meilisearchClientService.getKeys()
+        return keys.public
     }
 }
